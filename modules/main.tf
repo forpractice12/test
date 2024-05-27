9adadb4f4e977cf906596c0803e07761fb1538ca
sohailmask <sohailsohail45@gmail.com>
2024-05-27 11:17:08 +0530

    update

--- modules/main.tf
+++ modules/main.tf
@@ -1,17 +1,17 @@
 module  "s3"  {
-  source  = "./modules/s3"
+  source  = "./s3"
   for_each  = var.bucket
   bucket_name = each.value.bucket_name
 }
 
 module  "iam" {
-  source  = "./modules/iam"
+  source  = "./iam"
   for_each  = var.rolename
   rolename = each.value.rolename
 }
 
 module "vpc" {
-  source = "./modules/vpc"
+  source = "./vpc"
   cidr = var.cidr
   for_each  = var.vpc_name
   vpc_name = each.value.vpc_name
