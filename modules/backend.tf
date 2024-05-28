a68ba4d4daa85a8ce8569d1c4cc92272e3aed572
sohailmask <sohailsohail45@gmail.com>
2024-05-28 11:46:37 +0530

    update

--- modules/backend.tf
+++ modules/backend.tf
@@ -3,6 +3,5 @@ terraform {
     bucket         = "chidorichiki"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
-    dynamodb_table = "terraform_locks" // Optional: Use DynamoDB for locking
   }
 }
